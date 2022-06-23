f78c0e1b2294d94c6c38ec2c4d27150798938f4c
10011999 <motewaraditya77@gmail.com>
2022-06-23 10:07:25 +0530

    Added: Day4 snakeLadder

---
+++ SnakeLadderUcOne.java
@@ -0,0 +1,9 @@
+package SnakeLadder;
+
+public class SnakeLadderUcOne {
+    public static void main(String[] args) {
+        int pos = 0;
+        System.out.println("welcom a snake ladder");
+
+    }
+}
---
+++ SnakeLadderUcTwo.java
@@ -0,0 +1,10 @@
+package SnakeLadder;
+
+public class SnakeLadderUcTwo {
+    public static void main(String[] args) {
+        int pos=0;
+        System.out.println("Welcome to snake ladder");
+        int dice = (int) Math.floor(1 + Math.random() * 6);
+        System.out.println(dice);
+    }
+}
